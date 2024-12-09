f017a38b5ee67557f16dd87fffb79c125f5c9832
D4GUR4SU <douglas_search@hotmail.com>
2024-12-09 05:43:58 -0300

    Adding airixss to install

--- install.sh
+++ install.sh
@@ -1,74 +1,84 @@
 #!/usr/bin/env bash
 
-echo -e "\033[01;31m[+]\033[0m Starting Tools Instalations 🔧\n\n"
+RED="\033[01;31m"
+GREEN="\033[01;32m"
+BLUE="\033[01;34m"
+YELLOW="\033[01;33m"
+WHITE="\033[01;37m"
+RESET="\033[0m"
+
+echo -e "${RED}[+] Starting Tools Instalations 🔧\n\n${RESET}"
 
 go env -w GO111MODULE=auto
 
-echo -e "\033[01;31m[+]\033[0m Installing curl"
+echo -e "${GREEN}[+] Installing curl${RESET}"
 sudo apt install curl
 
-echo -e "\033[01;31m[+]\033[0m Installing subfinder"
+echo -e "${GREEN}[+] Installing subfinder${RESET}"
 go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
 
-echo -e "\033[01;31m[+]\033[0m Installing findomain"
+echo -e "${GREEN}[+] Installing findomain${RESET}"
 curl -LO https://github.com/findomain/findomain/releases/latest/download/findomain-linux.zip
 unzip findomain-linux.zip
 chmod +x findomain
 sudo mv findomain /usr/bin/findomain
 
-echo -e "\033[01;31m[+]\033[0m Installing assetfinder"
+echo -e "${GREEN}[+] Installing assetfinder${RESET}"
 go install github.com/tomnomnom/assetfinder@latest
 
-echo -e "\033[01;31m[+]\033[0m Installing chaos"
+echo -e "${GREEN}[+] Installing chaos${RESET}"
 go install -v github.com/projectdiscovery/chaos-client/cmd/chaos@latest
 
-echo -e "\033[01;31m[+]\033[0m Installing katana"
+echo -e "${GREEN}[+] Installing katana${RESET}"
 CGO_ENABLED=1 go install github.com/projectdiscovery/katana/cmd/katana@latest
 
-echo -e "\033[01;31m[+]\033[0m Installing httpx"
+echo -e "${GREEN}[+] Installing httpx${RESET}"
 go install -v github.com/projectdiscovery/httpx/cmd/httpx@latest
 
-echo -e "\033[01;31m[+]\033[0m Installing naabu"
+echo -e "${GREEN}[+] Installing naabu${RESET}"
 go install -v github.com/projectdiscovery/naabu/v2/cmd/naabu@latest
 
-echo -e "\033[01;31m[+]\033[0m Installing jq"
+echo -e "${GREEN}[+] Installing jq${RESET}"
 sudo apt-get install jq
 
-echo -e "\033[01;31m[+]\033[0m Installing anew"
+echo -e "${GREEN}[+] Installing anew${RESET}"
 go install -v github.com/tomnomnom/anew@latest
 
-echo -e "\033[01;31m[+]\033[0m Installing dnsx"
+echo -e "${GREEN}[+] Installing dnsx${RESET}"
 go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
 
-echo -e "\033[01;31m[+]\033[0m Installing mapcidr"
+echo -e "${GREEN}[+] Installing mapcidr${RESET}"
 go install -v github.com/projectdiscovery/mapcidr/cmd/mapcidr@latest
 
-echo -e "\033[01;31m[+]\033[0m Installing nuclei"
+echo -e "${GREEN}[+] Installing nuclei${RESET}"
 go install -v github.com/projectdiscovery/nuclei/v3/cmd/nuclei@latest
 
-echo -e "\033[01;31m[+]\033[0m Installing waybackurls"
+echo -e "${GREEN}[+] Installing waybackurls${RESET}"
 go install github.com/tomnomnom/waybackurls@latest
 
-echo -e "\033[01;31m[+]\033[0m Installing gf"
+echo -e "${GREEN}[+] Installing gf${RESET}"
 go install github.com/tomnomnom/gf
 
-echo -e "\033[01;31m[+]\033[0m Installing gau"
+echo -e "${GREEN}[+] Installing gau${RESET}"
 go install github.com/lc/gau/v2/cmd/gau@latest
 
-echo -e "\033[01;31m[+]\033[0m Installing jsubs"
+echo -e "${GREEN}[+] Installing jsubs${RESET}"
 GO111MODULE=on go get -u -v github.com/lc/subjs@latest
 
-echo -e "\033[01;31m[+]\033[0m Installing uro"
+echo -e "${GREEN}[+] Installing uro${RESET}"
 pipx install uro
 
-echo -e "\033[01;31m[+]\033[0m Installing nilo"
+echo -e "${GREEN}[+] Installing nilo${RESET}"
 go install github.com/ferreiraklet/nilo
 
-echo -e "\033[01;31m[+]\033[0m Installing qsreplace"
+echo -e "${GREEN}[+] Installing qsreplace${RESET}"
 go install github.com/tomnomnom/qsreplace@latest
 
-echo -e "\033[01;31m[+]\033[0m Installing dirsearch"
+echo -e "${GREEN}[+] Installing dirsearch${RESET}"
 sudo apt install dirsearch
 
-echo -e "\033[01;31m[+]\033[0m Installing gospider"
+echo -e "${GREEN}[+] Installing gospider${RESET}"
 GO111MODULE=on go install github.com/jaeles-project/gospider@latest
+
+echo -e "${GREEN}Installing airixss${RESET}"
+go install github.com/ferreiraklet/airixss@latest
